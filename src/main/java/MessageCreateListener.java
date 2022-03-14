c204ab79c4c1a65c0ea380fd5de496f7fe544ec9
Simon <simon.eklundh@gmail.com>
2022-03-14 23:17:17 +0100

    current version, needs to fix channel permissions, then rebuild to oop

--- src/main/java/MessageCreateListener.java
+++ src/main/java/MessageCreateListener.java
@@ -2,7 +2,6 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.ChannelCategory;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.channel.ServerTextChannelBuilder;
-import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.permission.PermissionType;
 import org.javacord.api.entity.permission.PermissionsBuilder;
 import org.javacord.api.entity.server.Server;
@@ -10,7 +9,6 @@ import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -40,27 +38,25 @@ public class MessageCreateListener implements org.javacord.api.listener.message.
 				Optional<User> b = event.getMessage().getUserAuthor();
 
 				if (!channels.containsKey(b.get().getId())) {
-					List<ChannelCategory> d = a.get().getChannelCategoriesByName("helpchannels");
-					if(d.isEmpty()) return;
-					ServerTextChannelBuilder e = a.get().createTextChannelBuilder().setName("help-" + b.get().getName().toLowerCase(Locale.ROOT)).setCategory(d.get(0));
-					//PermissionsBuilder f = new PermissionsBuilder().setAllowed(PermissionType.)
-					PermissionsBuilder g = new PermissionsBuilder();
-					g.setAllowed(PermissionType.SEND_MESSAGES).setAllowed(PermissionType.READ_MESSAGES).setAllowed(PermissionType.READ_MESSAGE_HISTORY);
-
-					e.addPermissionOverwrite(b.get(), g.build());
-					CompletableFuture<ServerTextChannel> f = e.create();
 					try {
+						ChannelCategory d = a.get().getChannelCategoriesByName("helpchannels").get(0);
+
+
+						ServerTextChannelBuilder e = a.get().createTextChannelBuilder().setName("help-" + b.get().getName().toLowerCase(Locale.ROOT));
+						e.setCategory(d);
+
+
+						CompletableFuture<ServerTextChannel> f = e.create();
+
 						ServerTextChannel h = f.get();
 						event.getChannel().sendMessage("join here " + h.getMentionTag());
 						channels.put(b.get().getId(), h.getId());
 						database.addChannel(b.get().getId(), h.getId());
-					} catch (InterruptedException | ExecutionException ex) {
+					} catch (InterruptedException | ExecutionException | IndexOutOfBoundsException ex) {
 						ex.printStackTrace();
 					}
 
-
-				}
-				else{
+				} else {
 
 					Optional<ServerTextChannel> x = api.getServerTextChannelById(channels.get(b.get().getId()));
 					x.ifPresent(serverTextChannel -> event.getChannel().sendMessage("join here " + serverTextChannel.getMentionTag()));
@@ -72,7 +68,7 @@ public class MessageCreateListener implements org.javacord.api.listener.message.
 			if (event.getServer().isPresent() && event.getMessage().getUserAuthor().isPresent()) {
 				Optional<Server> a = event.getServer();
 				Optional<User> b = event.getMessage().getUserAuthor();
-				if (channels.containsKey(b.get().getId())){
+				if (channels.containsKey(b.get().getId())) {
 					a.get().getChannelById(channels.get(b.get().getId())).get().delete();
 					database.removeChannel(b.get().getId(), channels.get(b.get().getId()));
 				}
