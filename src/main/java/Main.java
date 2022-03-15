f0c4c8b371d58533a1c85a573df84969b500ad4c
Simon <simon.eklundh@gmail.com>
2022-03-15 22:55:59 +0100

    swapping to Slashcommands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,7 @@ import java.util.HashMap;
 
 public class Main {
 
-private static HashMap<Long,Long> textChannelPerPerson = new HashMap<>();
-private static Database database = new Database();
+	private static final Database database = new Database();
 
 
 	public static void main(String[] args) {
@@ -17,7 +16,7 @@ private static Database database = new Database();
 
 		DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
-		textChannelPerPerson = database.getChannels();
+		HashMap<Long, Long> textChannelPerPerson = database.getChannels();
 		//api.addMessageCreateListener(new MessageCreateListener(textChannelPerPerson, database, api));
 		SlashCommand.with("help", "command for people who need help").createGlobal(api).join();
 		SlashCommand.with("thanks", "command for people who have received help").createGlobal(api).join();
